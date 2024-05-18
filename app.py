e6cc179ce431edc517dcb3b21ef66f6159f68035
User Evgeny <hodorevevgenij6@gmail.com>
2024-05-18 16:51:52 +0600

    form register and connect db

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import flask
+import psycopg2
 from data import db_session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from forms.login_form import LoginForm
 from forms.register_form import RegisterForm
 from forms.profile_form import ProfileForm
 from flask import Flask, render_template
+from data import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+con = psycopg2.connect(host='localhost', dbname='data', user='postgres', password='1234', port=5432)
 
 
 @app.route('/')
@@ -16,7 +21,10 @@ def main_window():  # put application's code here
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return render_template('/')
+    return render_template('register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,4 +37,5 @@ def login():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
     app.run()
--- data/__all_models.py
+++ data/__all_models.py
@@ -1,2 +1,1 @@
 from data.user import User
-from data.message import Message
